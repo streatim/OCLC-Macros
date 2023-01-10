316bb85191ff6e07a5b7eea556ed2876f27ddc84
Tim Streasick <66029458+streatim@users.noreply.github.com>
2023-01-10 18:37:46 -0500

    Update 336.vb
    Refactored 336 Macro.

--- 336.vb
+++ 336.vb
@@ -1,47 +1,12 @@
 'MacroName:336
-'MacroDescription:336-338
+'MacroDescription:Add fields 336 - 338 for text, unmediated, volume.
 
 Sub Main
-
    Dim CS As Object
    Set CS = CreateObject("Connex.Client")
-
-   CS.AddFieldLine 24,""
-
-   CS.InsertText "336"
-
-   CS.TabKey True
-
-   CS.TabKey True
-
-   CS.InsertText "test"
-
-   CS.BackSpace
-
-   CS.BackSpace
-
-   CS.InsertText "xt ßb txt ß2 rdacontent"
-
-   CS.AddFieldLine 25,""
-
-   CS.InsertText "337"
-
-   CS.TabKey True
-
-   CS.TabKey True
-
-   CS.InsertText "unmediated ßb n ß2 rdamedia"
-
-   CS.AddFieldLine 26,""
-
-   CS.InsertText "338"
-
-   CS.TabKey True
-
-   CS.TabKey True
-
-   CS.InsertText "volume ßb nc ß2 rdacarrier"
-
-   CS.AddFieldLine 27,""
+   
+   CS.AddField 1, "336  text ßb txt ß2 rdacontent"
+   CS.AddField 1, "337  unmediated ßb n ß2 rdamedia"
+   CS.AddField 1, "338  volume ßb nc ß2 rdacarrier"
 
 End Sub
