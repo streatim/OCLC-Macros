c1428b98a5c9d15dd098a1cdf963a90cb0a658b8
Tim Streasick <66029458+streatim@users.noreply.github.com>
2023-01-10 19:14:32 -0500

    Update streaming-video.vb
    Refactors streaming-video and revises description.

--- streaming-video.vb
+++ streaming-video.vb
@@ -1,19 +1,10 @@
 'MacroName:streaming-video
-'MacroDescription:streaming video url
+'MacroDescription:Adds most of a streaming video URL for Films on Demand.
 
 Sub Main
 
    Dim CS As Object
    Set CS = CreateObject("Connex.Client")
-
-   CS.AddFieldLine 22,""
-
-   CS.InsertText "856"
-
-   CS.InsertText "4"
-
-   CS.InsertText "0"
-
-   CS.InsertText "ßu http://library.umd.umich.edu/research/fwd.php?http://digital.films.com/PortalPlaylists.aspx?aid=1213&xtid=     ßz Access web version."
+   CS.AddField 1, "85640ßu http://library.umd.umich.edu/research/fwd.php?http://digital.films.com/PortalPlaylists.aspx?aid=1213&xtid=     ßz Access web version."
 
 End Sub
