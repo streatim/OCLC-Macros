cb4f1f8dd083217c2f78cc74c5dcc1c70ce682f5
Tim Streasick <66029458+streatim@users.noreply.github.com>
2023-01-10 19:18:01 -0500

    Create TOC.vb

---
+++ TOC.vb
@@ -0,0 +1,11 @@
+'MacroName:TOC
+'MacroDescription:Table of contents
+
+Sub Main
+
+   Dim CS As Object
+   Set CS = CreateObject("Connex.Client")
+
+   CS.InsertText "  ßz Access table of contents"
+
+End Sub
