0080de4b8c3e5d98990250cc68ae94105ba55982
Tim Streasick <66029458+streatim@users.noreply.github.com>
2023-01-10 19:12:10 -0500

    Update rpkgift.vb
    Refactors rpkgift and adds description.

--- rpkgift.vb
+++ rpkgift.vb
@@ -1,44 +1,13 @@
 'MacroName:rpkgift
-'MacroDescription:gift lines
+'MacroDescription:Adds Katrina Baetz-Matthews donation statement.
 
 Sub Main
 
    Dim CS As Object
    Set CS = CreateObject("Connex.Client")
 
-   CS.CursorRow = 18
-
-   CS.CursorPosition = 27
-
-   CS.CursorRow = 19
-
-   CS.CursorPosition = 124
-
-   CS.AddFieldLine 20,""
-
-   CS.InsertText "599"
-
-   CS.InsertText " "
-
-   CS.InsertText " "
-
-   CS.InsertText "Gift from Katrina Baetz-Matthews, 10"
-
-   CS.BackSpace
-
-   CS.BackSpace
-
-   CS.InsertText "2010."
-
-   CS.AddFieldLine 21,""
-
-   CS.InsertText "710"
-
-   CS.InsertText "2"
-
-   CS.InsertText " "
-
-   CS.InsertText "Library donation, 2010."
+   CS.AddField 1, "599  Gift from Katrina Baetz-Matthews, 2010"
+   CS.AddField 1, "7102 Library donation, 2010"
 
 End Sub
 
